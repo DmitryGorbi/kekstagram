72453b9ceb5cf31f10d512b2aafa04499380ad1e
DmitryGorbi <godmni@gmail.com>
2024-02-05 09:24:34 +0300

    made 1 function

--- js/main.js
+++ js/main.js
@@ -1,2 +1,8 @@
 const form = document.querySelector('.img-upload__form');
 console.log(Object.entries(form));
+
+function getMaxLength(str, maxLength) {
+  return str.length <= maxLength ? true : false;
+}
+
+alert(getMaxLength('Привет!', 6));
